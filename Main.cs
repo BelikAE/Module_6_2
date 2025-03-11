80422775b97be9abd7e7eb1199388fcd3b53c646
BelikAE <BelikAE@HOME-PC>
2025-03-11 20:15:24 +1000

    вы

--- Main.cs
+++ Main.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,25 @@ using System.Threading.Tasks;
 
 namespace Module_6_2
 {
-    [Transaction(TransactionMode.Manual)]
+    [TransactionAttribute(TransactionMode.Manual)]
     public class Main : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            TaskDialog.Show("Сообщение", "Тест");
+            UIDocument uIDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uIDoc.Document;
+
+            Reference reference = uIDoc.Selection.PickObject(ObjectType.Element, "Выберите группу объектов");
+            Element element = doc.GetElement(reference);
+            Group group = element as Group;
+
+            XYZ point = uIDoc.Selection.PickPoint("Выберите точку");
+
+            Transaction transaction = new Transaction(doc);
+            transaction.Start("Копирование группы объектов");
+            doc.Create.PlaceGroup(point, group.GroupType);
+            transaction.Commit();
+
             return Result.Succeeded;
         }
     }
